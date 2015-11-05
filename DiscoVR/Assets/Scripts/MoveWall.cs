608e5a201a8cd9e3c6e1bcfe2b17445838530fa9
thatonedude11 <thatonedude11@gmail.com>
2015-11-05 09:35:43 -0600

    added James's scene

---
+++ DiscoVR/Assets/Scripts/MoveWall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWall : MonoBehaviour {
+    public Transform target;
+    public float speed = 1.0f;
+
+	void Update ()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+}
