0ebd4bea9dc4943442ccb6d850c57ce75f7cf097
jmalz92 <malzjustin@yahoo.com>
2015-11-11 02:51:26 -0600

    Adding hub world and mountain scene

---
+++ DiscoVR/Assets/SphereCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCollision : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        Application.LoadLevel(1);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
